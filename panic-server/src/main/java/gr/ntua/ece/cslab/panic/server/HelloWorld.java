8f3ad08d8c0b12862ce76eb58b6be9cbff1df377
Giannis Giannakopoulos <giannisgiannak@gmail.com>
2014-03-25 15:14:56 +0200

    plain REST server configuration using Jersey

--- panic-server/src/main/java/gr/ntua/ece/cslab/panic/server/HelloWorld.java
+++ panic-server/src/main/java/gr/ntua/ece/cslab/panic/server/HelloWorld.java
@@ -1,8 +1,22 @@
 package gr.ntua.ece.cslab.panic.server;
 
+import com.sun.jersey.spi.container.servlet.ServletContainer;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 class HelloWorld {
 
-	public static void main(String[] args){
-		System.out.println("Hello world");
-	}
+	public static void main(String[] args) throws Exception{
+            ServletHolder holder = new ServletHolder(ServletContainer.class);
+            holder.setInitParameter("com.sun.jersey.config.property.resourceConfigClass", "com.sun.jersey.api.core.PackagesResourceConfig");
+            holder.setInitParameter("com.sun.jersey.config.property.packages", "gr.ntua.ece.cslab.panic.server.rest");//Set the package where the services reside
+            holder.setInitParameter("com.sun.jersey.api.json.POJOMappingFeature", "true");
+            
+            Server server = new Server(9999);
+            ServletContextHandler context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS);
+            context.addServlet(holder, "/*");
+            server.start();
+            server.join();
+        }
 }
