c3c2efc306d7512bb4ff33901c06aad9388a14ea
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2014-04-01 17:22:40 +0300

    added InputPoint bean

---
+++ panic-server/src/main/java/gr/ntua/ece/cslab/panic/server/containers/beans/InputPoint.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2014 Giannis Giannakopoulos.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package gr.ntua.ece.cslab.panic.server.containers.beans;
+
+import com.owlike.genson.annotation.JsonIgnore;
+import gr.ntua.ece.cslab.panic.server.shared.SystemLogger;
+import java.io.Serializable;
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.DecimalFormat;
+import java.util.Collection;
+import java.util.HashMap;
+
+/**
+ * Represents an input space point. 
+ * @author Giannis Giannakopoulos
+ */
+public class InputPoint implements Serializable{
+    
+    @JsonIgnore
+    private String doubleFormat;
+    private HashMap<String, Double> values;
+
+    public InputPoint() {
+        this.values = new HashMap<>();
+        doubleFormat = "0.";
+        for(int i=0;i<10;i++)
+            doubleFormat+="0";
+    }
+   
+    public HashMap<String, Double> getValues() {
+        return values;
+    }
+
+    public void setValues(HashMap<String, Double> values) {
+        for(String key : values.keySet()) {
+            values.put(key, chopDouble(values.get(key)));
+        }
+        this.values = values;
+    }
+
+    @JsonIgnore
+    public void addDimension(String key, Double value) {
+        this.values.put(key, chopDouble(value));
+    }
+    
+    @JsonIgnore
+    public Double getValue(String key) {
+        return this.values.get(key);
+    }
+    
+    @JsonIgnore
+    public Collection<Double> getValuesAsCollection() {
+        return this.values.values();
+    }
+    
+    @JsonIgnore
+    public Collection<String> getKeysAsCollection() {
+        return this.values.keySet();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        boolean result = super.equals(o);
+        InputPoint other;
+        if(o instanceof InputPoint) 
+            other = (InputPoint) o;
+        else
+            return result;
+        
+        for(String key : this.getKeysAsCollection()) {
+            if((other.getValue(key) == null) || !other.getValue(key).equals(this.getValue(key)))
+                return false;
+        }
+        return (other.getKeysAsCollection().size() == this.getKeysAsCollection().size());
+    }
+
+    @Override
+    public int hashCode() {
+        ByteBuffer buffer = ByteBuffer.allocate(this.values.size() * Double.SIZE/8);
+        for(String key : this.getKeysAsCollection())
+            buffer.putDouble(this.getValue(key));
+        try {
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            digest.reset();
+            digest.update(buffer);
+            byte[] md5 = digest.digest(buffer.array());
+            ByteBuffer buf = ByteBuffer.wrap(md5);
+            return buf.getInt();
+            
+        } catch (NoSuchAlgorithmException ex) {
+            SystemLogger.get().error(ex.getMessage());
+        }
+        return 1;
+    }
+    
+    private Double chopDouble(Double initial) {
+        return new Double(new DecimalFormat(this.doubleFormat).format(initial));
+        
+    }
+}
