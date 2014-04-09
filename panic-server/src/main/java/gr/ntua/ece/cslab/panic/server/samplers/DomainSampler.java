f681110e917b3a42a691810be255f881fda49db1
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2014-04-09 10:39:54 +0300

    added first sampling classes

---
+++ panic-server/src/main/java/gr/ntua/ece/cslab/panic/server/samplers/DomainSampler.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2014 Giannis Giannakopoulos.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package gr.ntua.ece.cslab.panic.server.samplers;
+
+import gr.ntua.ece.cslab.panic.server.containers.beans.InputSpacePoint;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Describes the API with which the objects will contact with the Samplers.
+ * @author Giannis Giannakopoulos
+ */
+public interface DomainSampler {
+    
+    /**
+     * This method is used to return the next point of the sampling space
+     * @return 
+     */
+    public InputSpacePoint next();
+    
+    /**
+     * Informs the sampler about the applicable values per dimension.
+     * @param ranges 
+     */
+    public void setDimensionsWithRanges(Map<String, List<Double>> ranges);
+}
