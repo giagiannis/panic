b9e24d6ec74c4f697c13d066d38d5de8225fc146
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2014-06-10 17:51:47 +0300

    added added first

---
+++ panic-deployer/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='panic-deployer',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='Apache License v2.0',
+    author='Giannis Giannakopoulos',
+    author_email='ggian@cslab.ece.ntua.gr',
+    description='Deployment tool used by PANIC profiler, deploying application over Okeanos'
+)
