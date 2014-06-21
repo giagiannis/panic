c038dbe7957eeeab945f1d38da81bb0737566ad9
Giannis Giannakopoulos <giannisgiannak@gmail.com>
2014-06-21 12:39:05 +0300

    added requirements

--- panic-deployer/setup.py
+++ panic-deployer/setup.py
@@ -4,7 +4,8 @@ setup(
     name="panic-deployer",
     version="0.1",
     packages=find_packages(),
-    requires=[],
+    requires=['kamaki',
+              'paramiko'],
     author='Giannis Giannakopoulos',
     author_email='ggian@cslab.ece.ntua.gr'
 )
