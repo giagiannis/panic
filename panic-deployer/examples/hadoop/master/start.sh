57d3cd1c73c4203e82b4b07c7579f3d461c0c670
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2014-07-25 13:32:56 +0300

    upadted examples

--- panic-deployer/examples/hadoop/master/start.sh
+++ panic-deployer/examples/hadoop/master/start.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 HADOOP_INSTALLATION_PATH="/opt/hadoop"
+HAMA_INSTALLATION_PATH="/opt/hama/"
 
 # Format namenode and start the hadoop cluster
 
 $HADOOP_INSTALLATION_PATH/bin/hadoop namenode -format
 
 $HADOOP_INSTALLATION_PATH/bin/start-dfs.sh
+sleep 30
 
 $HADOOP_INSTALLATION_PATH/bin/start-mapred.sh
 
-echo "hadoop started"
+$HAMA_INSTALLATION_PATH/bin/start-bspd.sh
--- panic-deployer/examples/hadoop/shared/configure.sh
+++ panic-deployer/examples/hadoop/shared/configure.sh
@@ -3,8 +3,12 @@
 # This scripts downloads and configures Hadoop.
 
 JAVA_HOME="/usr/lib/jvm/java-7-openjdk-amd64/"
+
 HADOOP_URL="http://apache.cc.uoc.gr/hadoop/common/stable1/hadoop-1.2.1-bin.tar.gz"
 HADOOP_INSTALLATION_PATH="/opt/hadoop"
+
+HAMA_URL="http://mirror.cogentco.com/pub/apache/hama/hama-0.6.4/hama-0.6.4.tar.gz"
+HAMA_INSTALLATION_PATH="/opt/hama/"
 
 download_hadoop(){
 echo "Downloading hadoop"
@@ -20,6 +24,19 @@ ln -s /usr/lib/libsnappy.so.1 /opt/hadoop/lib/native/Linux-amd64-64/libsnappy.so
 
 echo "Hadoop downloaded and extracted"
 }
+
+download_hama(){
+echo "Downloading hama"
+wget $HAMA_URL -O /tmp/hama.tar.gz -o /tmp/hama_wget_logs.txt
+tar xfz /tmp/hama.tar.gz -C /tmp/
+mv /tmp/hama-*/ $HAMA_INSTALLATION_PATH
+echo "export PATH=\$PATH:/opt/hama/bin/" >> /etc/profile
+rm /tmp/hama.tar.gz
+
+echo "Hama downloaded and extracted"
+
+}
+
 
 conf_xml(){
 sed -i -e "/<configuration>/a \ \t<property>\n\t\t<name>$1</name>\n\t\t<value>$2</value>\n\t</property>" $3
@@ -57,5 +74,33 @@ conf_xml mapred.child.java.opts -Xmx${MEMORY_MB}m $HADOOP_INSTALLATION_PATH/conf
 echo "Hadoop configured"
 }
 
+configure_hama(){
+# the master must ask the slave for cores
+NUMBER_OF_CORES=$(ssh slave1 "cat /proc/cpuinfo | grep processor | wc -l")
+MEMORY_MB=$(free -m | grep -i mem | awk '{print$2}')
+SLAVES=$(cat /etc/hosts | grep slave | wc -l)
+
+# set groomeservers
+echo -n >$HAMA_INSTALLATION_PATH/conf/groomservers
+for i in $(seq 1 $SLAVES); do
+echo slave$i >> $HAMA_INSTALLATION_PATH/conf/groomservers
+done
+
+# configure hama-env.sh
+sed  -i 's|# export JAVA_HOME.*|export JAVA_HOME='"$JAVA_HOME"'|' $HAMA_INSTALLATION_PATH/conf/hama-env.sh
+sed  -i 's|# export HAMA_MANAGES_ZK=true|export HAMA_MANAGES_ZK=true|' $HAMA_INSTALLATION_PATH/conf/hama-env.sh
+
+# configure hama-site.xml
+conf_xml fs.default.name hdfs://master1:9000 $HAMA_INSTALLATION_PATH/conf/hama-site.xml
+conf_xml hama.zookeeper.quorum master1,slave1,slave2 $HAMA_INSTALLATION_PATH/conf/hama-site.xml
+conf_xml bsp.master.address master1:40000 $HAMA_INSTALLATION_PATH/conf/hama-site.xml
+conf_xml bsp.tasks.maximum $NUMBER_OF_CORES $HAMA_INSTALLATION_PATH/conf/hama-site.xml
+conf_xml bsp.child.java.opts -Xmx${MEMORY_MB}m $HAMA_INSTALLATION_PATH/conf/hama-site.xml
+
+
+}
+
 download_hadoop
+download_hama
 configure_hadoop
+configure_hama
