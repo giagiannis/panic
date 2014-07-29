a6ec3390ad3318f9bde46250002c19b46b108d29
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2014-07-29 14:11:54 +0300

    added deployments and updated scripts

--- panic-deployer/examples/hadoop/master/run_benchmarks.sh
+++ panic-deployer/examples/hadoop/master/run_benchmarks.sh
@@ -10,8 +10,7 @@ HAMA_BIN="$HAMA_INSTALLATION_PATH/bin/hama"
 HADOOP_JAR="$HADOOP_INSTALLATION_PATH/hadoop-examples-1.2.1.jar"
 HAMA_JAR="$HAMA_INSTALLATION_PATH/hama-examples-0.6.4.jar"
 
-ADULT_DATASET_URL="http://snf-562459.vm.okeanos.grnet.gr/adult.dat"
-ADULT_DATASET_URL_SORT="http://snf-562459.vm.okeanos.grnet.gr/adult_sort.dat"
+DATASET_URL="http://snf-562459.vm.okeanos.grnet.gr/"
 
 TIMEFILE="/tmp/times.csv"
 LOGS="/tmp/jobs.log"
@@ -21,40 +20,46 @@ echo -n > $LOGS
 
 TIME_CMD="/usr/bin/time -f %e\t%U\t%S -o $TIMEFILE --append"
 
-# # Terasort benchmark
-# for MILLIONS in 10 20 30 40 50; do 
-# 	echo -ne "Teragen-${MILLIONS}\t" >>$TIMEFILE
-# 	$TIME_CMD $HADOOP_BIN jar $HADOOP_JAR teragen $[MILLIONS*10**6] /data/tera-${MILLIONS}M 1>>$LOGS 2>>$LOGS
-# 	tail -n 1 $TIMEFILE
-# 	echo -ne "Terasort-${MILLIONS}\t" >>$TIMEFILE
-# 	$TIME_CMD $HADOOP_BIN jar $HADOOP_JAR terasort /data/tera-${MILLIONS}M /output/tera-${MILLIONS}M 1>>$LOGS 2>>$LOGS
-# 	tail -n 1 $TIMEFILE
-# 	$HADOOP_BIN fs -rmr /data/tera-${MILLIONS}M /output/tera-${MILLIONS}M 1>>$LOGS 2>>$LOGS
-# done
-# 
-# 
-# # Pagerank benchmark
-# EDGES=50
-# for THOUSANDS in 50 60 70 80 90 100; do
-# 	echo -ne "Fastgen-${THOUSANDS}-${EDGES}\t" >> $TIMEFILE
-# 	$TIME_CMD $HAMA_BIN jar $HAMA_JAR gen fastgen $[THOUSANDS*1000] $EDGES /data/pagerank-${THOUSANDS}-${EDGES} 1 1>>$LOGS 2>>$LOGS
-# 	tail -n 1 $TIMEFILE
-# 	echo -ne "PageRank-${THOUSANDS}-${EDGES}\t" >> $TIMEFILE
-# 	$TIME_CMD $HAMA_BIN jar $HAMA_JAR pagerank /data/pagerank-${THOUSANDS}-${EDGES} /output/pagerank-${THOUSANDS}-${EDGES} 1000 1>>$LOGS 2>>$LOGS
-# 	tail -n 1 $TIMEFILE
-# 	$HADOOP_BIN fs -rmr /data/pagerank-${THOUSANDS}-${EDGES} /output/pagerank-${THOUSANDS}-${EDGES} 1>>$LOGS 2>>$LOGS
-# done
+
+
+hadoop fs -mkdir /data/
+# Terasort benchmark
+for MILLIONS in 10 20 30 40 50; do 
+	curl $DATASET_URL/tera-${MILLIONS}M.txt.gz | gzip -d | hadoop fs -put - /data/tera-${MILLIONS}M.txt
+	echo -ne "Terasort-${MILLIONS}\t" >>$TIMEFILE
+	$TIME_CMD $HADOOP_BIN jar $HADOOP_JAR terasort /data/ /output/tera-${MILLIONS}M 1>>$LOGS 2>>$LOGS
+	tail -n 1 $TIMEFILE
+	$HADOOP_BIN fs -rmr /data/tera-${MILLIONS}M.txt /output/tera-${MILLIONS}M 1>>$LOGS 2>>$LOGS
+done
+
+
+
+# Pagerank benchmark
+EDGES=50
+for THOUSANDS in 50 60 70 80 90 100; do
+ 	curl $DATASET_URL/page-${THOUSANDS}k.txt.gz | gzip -d | hadoop fs -put - /data/page-${THOUSANDS}k.txt
+	echo -ne "PageRank-${THOUSANDS}-${EDGES}\t" >> $TIMEFILE
+	$TIME_CMD $HAMA_BIN jar $HAMA_JAR pagerank /data/page-${THOUSANDS}k.txt /output/pagerank-${THOUSANDS}-${EDGES} 1000 1>>$LOGS 2>>$LOGS
+	tail -n 1 $TIMEFILE
+	$HADOOP_BIN fs -rmr /data/page-${THOUSANDS}k.txt /output/pagerank-${THOUSANDS}-${EDGES} 1>>$LOGS 2>>$LOGS
+done
 
 
 # K means benchmark
 
 # curl $ADULT_DATASET_URL | $HADOOP_BIN fs -put - /adult.dat
-curl $ADULT_DATASET_URL_SORT | $HADOOP_BIN fs -put - /adult_sort.dat
+# curl $ADULT_DATASET_URL_SORT | $HADOOP_BIN fs -put - /adult_sort.dat
+# 
+# CLUSTERS=100
+# for CLUSTERS in 40 50 60 70 80; do
+# 	echo -ne "Kmeans-${CLUSTERS}\t" >> $TIMEFILE
+# 	$TIME_CMD $HAMA_BIN jar $HAMA_JAR kmeans /adult_sort.dat /output  100 $CLUSTERS 1>>$LOGS 2>>$LOGS
+# 	tail -n 1 $TIMEFILE
+# done
 
-CLUSTERS=100
-for CLUSTERS in 40 50 60 70 80; do
-	echo -ne "Kmeans-${CLUSTERS}\t" >> $TIMEFILE
-	$TIME_CMD $HAMA_BIN jar $HAMA_JAR kmeans /adult_sort.dat /output  100 $CLUSTERS 1>>$LOGS 2>>$LOGS
-	tail -n 1 $TIMEFILE
-done
+
+echo "Benchmark results are ready and can be found in $TIMEFILE"
+
+cat $TIMEFILE
+
 exit 0
