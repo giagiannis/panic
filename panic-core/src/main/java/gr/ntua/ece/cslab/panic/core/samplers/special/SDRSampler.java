6bb0df298f64934c58dbf2f95b7045b82886b465
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2015-05-06 17:20:54 +0300

    foo

---
+++ panic-core/src/main/java/gr/ntua/ece/cslab/panic/core/samplers/special/SDRSampler.java
@@ -0,0 +1,9 @@
+package gr.ntua.ece.cslab.panic.core.samplers.special;
+
+/**
+ * Sufficient Dimensions Reduction Sampler.
+ * @author Giannis Giannakopoulos
+ */
+public class SDRSampler {
+    
+}
