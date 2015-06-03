a36149af47e22eb231a1a8f9bd1183b8be25c882
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2015-06-03 15:08:22 +0300

    restaged FactorAnalysis package and deleted unwanted files

--- panic-core/src/main/java/gr/ntua/ece/cslab/panic/core/containers/beans/InputSpaceStep.java
+++
@@ -1,9 +0,0 @@
-package gr.ntua.ece.cslab.panic.core.containers.beans;
-
-/**
- * This class represents a (multi dimensional) step over the InputSpacePoint.
- * @author Giannis Giannakopoulos
- */
-public class InputSpaceStep extends InputSpacePoint {
-    
-}
