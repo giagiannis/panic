6e42ce4cb49e5f42810bb6ec6cf71ce5ae530f29
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2016-02-15 11:34:04 +0200

    first commits for parsing conf file

--- panic-core/src/main/java/gr/ntua/ece/cslab/panic/core/fresh/client/EntryPoint.java
+++ panic-core/src/main/java/gr/ntua/ece/cslab/panic/core/fresh/client/EntryPoint.java
@@ -17,13 +17,84 @@
 
 package gr.ntua.ece.cslab.panic.core.fresh.client;
 
+import org.apache.commons.cli.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
 /**
  * Class used to execute experiments.
  * Created by Giannis Giannakopoulos on 2/11/16.
  */
 public class EntryPoint {
 
-    public static void main(String[] args) {
+    public static boolean DEBUG=false;
 
+
+    protected static void debugPrint(String message) {
+        if(DEBUG) {
+            System.out.println(message);
+        }
+    }
+    /**
+     * Parses the CLI options and returns them into a HashMap. If help has been requested, it prints the menu
+     * and exits
+     */
+    protected static Map<String, String> parseCLIOptions(String[] args) throws ParseException {
+        // setting the options
+        Options options = new Options();
+        options.addOption("h","help", false,"Prins this menu");
+        options.addOption("c", "conf", true, "overrides the configuration file that is, by default into the classpath");
+        options.getOption("c").setArgName("config");
+        options.addOption(null, "debug", false, "if set, prints diagnostic messages");
+
+
+        // parsing from args
+        CommandLineParser parser = new GnuParser();
+        CommandLine cline = parser.parse(options, args);
+
+        // creating conf HashMap
+        HashMap<String, String> kv = new HashMap<>();
+        for(Option o:cline.getOptions()) {
+            kv.put(o.getLongOpt(), o.getValue());
+        }
+
+        // setting parameters from CLI options
+        if(kv.containsKey("help")) { // quick and dirty
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp(EntryPoint.class.toString(), options);
+            System.exit(0);
+        }
+
+        if(kv.containsKey("debug")) {
+            DEBUG = true;
+        }
+        return kv;
+    }
+
+    protected static Properties loadConfigurationFile(Map<String,String> cliOptions) throws IOException {
+        String confFileName = "panic.properties";
+        Properties prop = new Properties();
+        InputStream stream;
+        if(cliOptions.containsKey("conf")) {
+            confFileName = cliOptions.get("conf");
+            debugPrint("Loading configuration file "+confFileName+" from filesystem");
+            stream = new FileInputStream(confFileName);
+        } else {
+            debugPrint("Loading configuration file "+confFileName+" from classpath");
+            stream = EntryPoint.class.getClassLoader().getResourceAsStream(confFileName);
+        }
+        prop.load(stream);
+        debugPrint("Conf file loaded and parsed");
+        return prop;
+    }
+
+    public static void main(String[] args) throws ParseException, IOException {
+        Map<String,String> cliOptions= parseCLIOptions(args);
+        Properties p = loadConfigurationFile(cliOptions);
     }
 }
